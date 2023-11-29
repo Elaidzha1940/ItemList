70eb02b97f0ffe02118dedd7c0cee52e9b1c3c3c
Elaidzha1940 <eli080293@gmail.com>
2023-11-30 02:44:38 +0300

    ItemList

---
+++ ItemList/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: ItemList
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 30.11.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ItemList/ItemListApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: ItemList
+//  File: ItemListApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 30.11.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct ItemListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
